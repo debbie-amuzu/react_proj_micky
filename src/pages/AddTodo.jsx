8d9eb739de7f33edf86e2f4bbd7f54308f87764f
Deborah Amuzu <debamuzudeb@gmail.com>
2024-10-03 15:41:35 +0000

    update

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddTodo = () => {
 
 const navigate = useNavigate();
 
-  const saveTodo = (event) => {
+  const saveTodo = async (event) => {
     event.preventDefault();
+    // collect form input
+    const formData = new FormData(event.target)
     // post data to todo api
+await axios.post("https://todo-api-g892.onrender.com/todos", {
+  title: formData.get('todo')
+})
+
+
     // Goto the homepage
     navigate('/');
   };
@@ -16,11 +24,7 @@ const navigate = useNavigate();
       <h1>add a new todo</h1>
       <form onSubmit={saveTodo}>
         <input
-          className="border "
-          type="text"
-          placeholder="Enter your task"
-          required
-        />
+          className="border " type="text" placeholder="Enter your task" required name="todo"/>
         <button type="submit">submit</button>
       </form>
       
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import TodoTile from "../components/TodoTile";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
-
-
+// useState ([]) is default for array if its object  useState ({})  useState (false) for boolean  useState (0) for numbers
 
 const Todos = () => {
-    return (
-        <div>
-            <h1>all todos</h1>
-            <div >
-                <TodoTile title= {"creating React app"}/>
-                <TodoTile title= {"Install Tailwind CSS"}/>
-                <TodoTile title= {"Commit code to Github"}/>
-                <TodoTile title= {"Host on Netify"}/>
-            </div>
-           <Link to={'/add'}>Add Todo</Link>
-        </div>
-    );
+  // 1. declare state to store variables
+  const [todos, setTodos] = useState([]);
 
-}
+  // 2. define a function to get todos
+  const getTodos = async () => {
+    // use axios to get the todos
+    const response = await axios.get(
+      "https://todo-api-g892.onrender.com/todos"
+    );
+    console.table(response.data);
+    // update todos state
+    setTodos(response.data);
+  };
+
+  // call the function with use effect
+  useEffect(() => {
+    getTodos();
+  }, []);
+  return (
+    <div>
+      <h1>all todos</h1>
+      <div>
+        {todos.map((todo) => {
+          return <TodoTile title={todo.title} key={todo._id} />;
+        })}
+      </div>
+      <Link to={"/add"}>Add Todo</Link>
+    </div>
+  );
+};
 
 export default Todos;
